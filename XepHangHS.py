10f11797948ece177833e8ec00d1fcd5fe8fbe65
TranNhatHan <91483520+TranNhatHan@users.noreply.github.com>
2021-10-07 07:13:42 +0700

    Add files via upload

---
+++ XepHangHS.py
@@ -0,0 +1,12 @@
+a = float(input("Nhap so diem trung binh:"))
+if a >= 9:
+    print("Xuat sac")
+elif a>= 8:
+    print("Gioi")
+elif a>=7:
+    print("Kha")
+elif a>=5:
+    print("Trung binh")
+elif a>=0:
+    print("Yeu")
+
